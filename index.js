4b03705f6aa1ea91aa686b8c3b6c2f5e3cef1632
RicardoIsaac <105662685+RicardoIsaac@users.noreply.github.com>
2023-03-07 23:38:41 -0600

    Base de la configuracion del index

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-const express= require("express");
-const app= express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081",
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());//  middleware permite analizxar la solicitud en un fomato Json
+
+app.use(bodyParser.urlencoded({ extended: true }));// middleware permite analizxar la solicitud en un fomato codificado url
+
+app.get("/", (req, res) => {
+    res.json({ message: "connection with express" });
+  });// ruta de prueba
 
 app.use(express.json());
-const port = process.env.PORT||8080;
-app.listen(port,()=>console.log(`Listening on port ${port}`))
+const port = process.env.port || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}`));
