34cf1184a22428a7308bea9192b2261863aa00f5
RicardoIsaac <105662685+RicardoIsaac@users.noreply.github.com>
2023-03-09 11:24:33 -0600

    image and adress update

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ app.get("/", (req, res) => {
 
   require('./routes/auth.routes')(app);
   require('./routes/user.routes')(app);
+  require("./routes/product.routes")(app)
 
 app.use(express.json());
 const port = process.env.port || 8080;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -29,4 +29,10 @@ module.exports = function(app) {
       [authJwt.verifyToken, authJwt.isAdmin],
       controller.adminBoard
     );
+
+    app.put(
+      "/api/test/update",
+      [authJwt.verifyToken],
+      controller.updateuser
+      )
   };
